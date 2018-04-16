e986080309dd1f853a8a842a4e4efb35a959af64
Thomas Stenius <thomas.stenius@helsinki.fi>
2018-04-17 00:51:47 +0300

    New UI + gameplay

--- Minesweeper/src/main/java/minesweeper/ui/MinesweeperUi.java
+++ Minesweeper/src/main/java/minesweeper/ui/MinesweeperUi.java
@@ -1,14 +1,22 @@
 package minesweeper.ui;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Parent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import minesweeper.game.MinesweeperGame;
 import minesweeper.game.Square;
 
@@ -18,10 +26,13 @@ public class MinesweeperUi extends Application {
     final static int SQUARE_SIZE = 40;
     Scene scene;
     Pane mainMenu;
+    Pane gridPane;
+    int squaresX;
+    int squaresY;
 
     // tässä luodaan ruudukko
-    public Pane createGrid(int squaresX, int squaresY) {
-        Pane gridPane = new Pane();
+    public Pane createGrid() {
+        gridPane = new Pane();
         gridPane.setPrefSize(SQUARE_SIZE * squaresX, SQUARE_SIZE * squaresY);
 
         //ensin luodaan kenttä (logiikkatasolla), ja plaseerataan pommit satunnaisesti.
@@ -35,7 +46,19 @@ public class MinesweeperUi extends Application {
             for (int x = 0; x < squaresX; x++) {
 
                 SquarePane squarePane = new SquarePane(grid[x][y], SQUARE_SIZE);
-                squarePane.setOnMouseClicked(e -> click(squarePane));
+
+                squarePane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+                    @Override
+                    public void handle(MouseEvent event) {
+                        MouseButton btn = event.getButton();
+                        if (btn == MouseButton.PRIMARY) {
+                            leftClick(squarePane);
+                        } else if (btn == MouseButton.SECONDARY) {
+                            rightClick(squarePane);
+                        }
+                    }
+                });
 
                 int b = squarePane.getSquare().getAdjacentBombs();
 
@@ -50,30 +73,64 @@ public class MinesweeperUi extends Application {
         return gridPane;
     }
 
-    public void click(SquarePane sqPane) {
-
-        // Jos ruutu on pommi, hypätään takaisin päävalikkoon.
-        if (sqPane.getSquare().isBomb()) {
-
-            sqPane.getEdge().setFill(Color.RED);
-            sqPane.getText().setVisible(true);
-            System.out.println("Hähää, hävisit.");
-            scene.setRoot(mainMenu);
-
-            return;
-        }
+    public void leftClick(SquarePane sqPane) {
 
         // Jos ruutua on jo klikattu, ei tehdä mitään.
         if (sqPane.isOpen()) {
             return;
         }
 
+        // Jos ruutu on pommi, hypätään takaisin päävalikkoon.
+        if (sqPane.getSquare().isBomb()) {
+
+            revealAllBombs();
+            System.out.println("Hähää, hävisit.");
+            Timeline timeline = new Timeline(new KeyFrame(
+                    Duration.millis(2000),
+                    ae -> scene.setRoot(mainMenu)));
+            timeline.play();
+
+            return;
+        }
+
         // Muissa tapauksissa paljastetaan alla oleva teksti (numero) ja
         // värjätään tausta valkoiseksi, niin että numero erottuu selvästi.
         sqPane.setOpen(true);
-        sqPane.getEdge().setFill(Color.WHITE);
-        sqPane.getText().setVisible(true);
+        sqPane.showEdge(false);
 
+    }
+
+    private void rightClick(SquarePane sqPane) {
+        
+        // Jos ruutua on jo auki, ei tehdä mitään.
+        if (sqPane.isOpen()) {
+            return;
+        }
+        
+        if (sqPane.getFlag().isVisible()) {
+            sqPane.showFlag(false);
+            return;
+        }
+        
+        if (!sqPane.getFlag().isVisible()) {
+            sqPane.showFlag(true);
+        }
+    }
+
+    public void revealAllBombs() {
+        for (int i = 0; i < (squaresX * squaresY); i++) {
+            Node n = gridPane.getChildren().get(i);
+            SquarePane squarePane = (SquarePane) n;
+
+            // Avataan kaikki ruudut siksi, että silloin niitä ei voi enää avata
+            // klikkaamalla. Muuten voisi jatkaa pelaamista pelin päätyttyään.
+            squarePane.getSquare().setIsOpen(true);
+
+            if (squarePane.getSquare().isBomb()) {
+                squarePane.getTile().setFill(Color.RED);
+                squarePane.showEdge(false);
+            }
+        }
     }
 
     @Override
@@ -86,12 +143,21 @@ public class MinesweeperUi extends Application {
         playButton.setOnAction(e -> {
 
             //kun klikkaa play-nappulaa, luo pelinäytön
-            game = new MinesweeperGame(20, 20);
-            scene.setRoot(createGrid(20, 20));
+            squaresX = 20;
+            squaresY = 20;
+            game = new MinesweeperGame(squaresX, squaresY);
+//            start.setWidth(SQUARE_SIZE * squaresX);
+//            start.setHeight(SQUARE_SIZE * squaresY);
+            scene.setRoot(createGrid());
         });
 
         mainMenu = new StackPane();
         mainMenu.getChildren().add(playButton);
+
+//        BorderPane border = new BorderPane();
+//        HBox hbox = new HBox(800);
+//        border.setLeft(hbox);
+//        border.setCenter(mainMenu);
         scene = new Scene(mainMenu, SQUARE_SIZE * 20, SQUARE_SIZE * 20);
 
         //aloitusnäytön setup
--- Minesweeper/src/main/java/minesweeper/ui/SquarePane.java
+++ Minesweeper/src/main/java/minesweeper/ui/SquarePane.java
@@ -2,39 +2,93 @@ package minesweeper.ui;
 
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import minesweeper.game.Square;
 
 // Tässä luokassa määritellään Square-oliolle graafisia ominaisuuksia.
 // Käytetään StackPanea, koska sen voi helposti lisätä MinesweeperUi-luokan gridPaneen.
 public class SquarePane extends StackPane {
-    
+
     private Square square;
+    private Rectangle tile;
     private Rectangle edge;
+    private Polygon flag;
+    private Rectangle flagpole;
+    private Rectangle flagbase;
     private Text text = new Text();
     private boolean isOpen = false;
+    private int SQUARE_SIZE;
 
     public SquarePane(Square square, int SQUARE_SIZE) {
-
+        this.SQUARE_SIZE = SQUARE_SIZE;
         this.square = square;
-        edge = new Rectangle(SQUARE_SIZE - 2, SQUARE_SIZE - 2);
-        edge.setFill(Color.web("0x2b2b2b"));
-        edge.setStroke(Color.web("0x828282"));
 
-        text.setFont(Font.font(18));
+        setUpSquareGraphics();
+        setUpFlagGraphics();
+
+        getChildren().add(tile);
+        getChildren().add(text);
+        getChildren().add(edge);
+        getChildren().add(flagbase);
+        getChildren().add(flagpole);
+        getChildren().add(flag);
+        setTranslateX(square.getX() * SQUARE_SIZE);
+        setTranslateY(square.getY() * SQUARE_SIZE);
+    }
+
+    private void setUpSquareGraphics() {
+        tile = new Rectangle(SQUARE_SIZE, SQUARE_SIZE);
+        tile.setFill(Color.web("0xcccccc"));
+        tile.setStroke(Color.web("0x828282"));
+
+        edge = new Rectangle(SQUARE_SIZE - 4, SQUARE_SIZE - 4);
+        edge.setFill(Color.web("0xcccccc"));
+        edge.setStrokeWidth(4);
+        edge.setStroke(Color.web("0xf2f2f2"));
+
+        text.setFont(Font.font("Verdana", FontWeight.BOLD, 18));
+        
         if (square.isBomb()) {
             text.setText("X");
         } else {
             text.setText("");
         }
-        text.setVisible(false);
+//        text.setVisible(false);
+    }
 
-        getChildren().add(edge);
-        getChildren().add(text);
-        setTranslateX(square.getX() * SQUARE_SIZE);
-        setTranslateY(square.getY() * SQUARE_SIZE);
+    private void setUpFlagGraphics() {
+        flag = new Polygon();
+        flag.getPoints().addAll(new Double[]{
+            0.0, 10.0,
+            15.0, 0.0,
+            15.0, 15.0
+        });
+        flag.setTranslateY(-8);
+        flag.setTranslateX(-6);
+        flag.setFill(Color.RED);
+        flag.setVisible(false);
+
+        flagpole = new Rectangle(3, SQUARE_SIZE * 0.5);
+        flagpole.setFill(Color.RED);
+        flagpole.setVisible(false);
+
+        flagbase = new Rectangle(SQUARE_SIZE * 0.5, SQUARE_SIZE * 0.1);
+        flagbase.setTranslateY(12);
+        flagbase.setVisible(false);
+    }
+
+    public void showFlag(Boolean value) {
+        flag.setVisible(value);
+        flagpole.setVisible(value);
+        flagbase.setVisible(value);
+    }
+
+    public void showEdge(Boolean value) {
+        edge.setVisible(value);
     }
 
     public boolean isOpen() {
@@ -52,11 +106,19 @@ public class SquarePane extends StackPane {
     public Square getSquare() {
         return square;
     }
+    
+    public Polygon getFlag() {
+        return flag;
+    }
+
+    public Rectangle getTile() {
+        return tile;
+    }
 
     public void setOpen(boolean bool) {
         isOpen = bool;
     }
-    
+
     public void setText(String text) {
         this.text.setText(text);
     }
